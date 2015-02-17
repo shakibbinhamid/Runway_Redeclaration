a81bc27a6c0a02858a388afec28bee470f218eaa
stefanjcollier <sc22g13@soton.ac.uk>
2015-02-17 17:49:24 +0000

    Added AirportInterface

---
+++ Runway_Redeclaration/src/AirportInterface.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @author Stefan
+ * @Editor 
+ *
+ */
+public interface AirportInterface {
+	
+	AirfieldInterface getAirfields();
+	
+	/**
+	 * The name of the airport
+	 * e.g. "Heathrowe Airport"
+	 */
+	String getName();
+	
+	
+}
