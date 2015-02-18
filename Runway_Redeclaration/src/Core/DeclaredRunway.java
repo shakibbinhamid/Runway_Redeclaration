86aad3aafc17ca906d8940e50372d7029674a3a3
Stefan <Stefan@152.78.102.18>
2015-02-18 14:14:12 +0000

    More private set methods in DeclaredRunway Changed DistancDeclarationException to VariableDeclarationException Added char constructor to VariableDeclarationException

--- Runway_Redeclaration/src/Core/DeclaredRunway.java
+++ Runway_Redeclaration/src/Core/DeclaredRunway.java
@@ -1,7 +1,8 @@
 package Core;
 
+import CoreInterfaces.AirfieldInterface;
 import CoreInterfaces.DeclaredRunwayInterface;
-import Exceptions.DistanceDeclarationException;
+import Exceptions.VariableDeclarationException;
 
 /**
  * This so far is a stub for editing
@@ -21,20 +22,44 @@ public class DeclaredRunway implements DeclaredRunwayInterface{
 
 	private char direction;
 
-	public DeclaredRunway(double tora, double disThr, double stop, double clear, int ang, char dirc) throws DistanceDeclarationException{
-		setTORA(tora);
-		setDisplacedThreshold(disThr);
-		setStopway(stop);
-		setClearway(clear);
+	/**
+	 * 
+	 * @param runway - The airfield with the runway on it that you want to redeclare
+	 * @param displacedThreshold - The user defined threshold to start from
+	 * @param angleFromNorth - The angle (in degrees) divided by 10 and rounded to the nearest 10
+	 * @param leftOrRight - Which side of the runway the plane will start from/take off to
+	 * 
+	 * @throws VariableDeclarationException is thrown when an invalid distance variable is declared
+	 */
+	public DeclaredRunway(AirfieldInterface runway, double displacedThreshold ,int angleFromNorth, char leftOrRight) throws VariableDeclarationException{
+		
+		setDisplacedThreshold(displacedThreshold);
+		if(!runway.hasObstacle()){
+			
+			setTORA(runway.getRunwayLength());
+			setStopway(runway.getInitialStopway());
+			
+			double clearway = getStopway()+runway.getClearedLength();
+			setClearway(clearway);
+			
+		}else{
+			//These are garuneteed to fail, as we have not handled the obstacle scenarios yet!
+			//TODO handle an obstacle
+			setTORA(-1);
+			setStopway(-1);
+			setClearway(-1);
+		}
+		//TODO include final check: TORA <= ASDA <= TODA 
 
 		ascentAngle = DeclaredRunwayInterface.DEFAULT_ANGLE_OF_ASCENT;
 		descentAngle = DeclaredRunwayInterface.DEFAULT_ANGLE_OF_DESCENT;
 
-		angle = ang;
-		direction = dirc;
+		angle = angleFromNorth;
+		direction = leftOrRight;
 	}
 
 //====[ Direction Methods  ]=====================================
+//----[ Getters ]------------------------------------------------------
 	@Override
 	public char getDirection() {
 		return direction;
@@ -54,7 +79,23 @@ public class DeclaredRunway implements DeclaredRunwayInterface{
 		out += String.valueOf(angle)+direction;
 		return out;
 	}
-
+//----[ Setters ]------------------------------------------------------
+	private void setDirection(char leftOrRight) throws VariableDeclarationException{
+		//If LoR is not L or R
+		if ( !(leftOrRight == 'L' || leftOrRight == 'R') )
+			throw new VariableDeclarationException("Direction",leftOrRight,"Must be 'L' or 'R'");
+		
+		this.direction = leftOrRight;
+	}
+	
+	public void setAngle(int angle) {
+		while(angle<0){
+			angle += 36;
+		}
+		this.angle = angle%36;
+	}
+	
+	
 //====[ Inert Direction Methods  ]=====================================
 //----[ Getters ]------------------------------------------------------
 	@Override
@@ -77,25 +118,25 @@ public class DeclaredRunway implements DeclaredRunwayInterface{
 		return disThreshold;
 	}
 //----[ Setters ]---------------------------------------------------------
-	private void setTORA(double tora) throws DistanceDeclarationException{
-		if(tora <= 0) throw new DistanceDeclarationException("TORA", tora, "TORA > 0");
+	private void setTORA(double tora) throws VariableDeclarationException{
+		if(tora <= 0) throw new VariableDeclarationException("TORA", tora, "TORA > 0");
 
 		this.decTora = tora;
 	}
 
-	private void setDisplacedThreshold(double threshold) throws DistanceDeclarationException{
-		if(threshold < 0) throw new DistanceDeclarationException("Displaced Threshold", threshold, "Threshold > 0");
+	private void setDisplacedThreshold(double threshold) throws VariableDeclarationException{
+		if(threshold < 0) throw new VariableDeclarationException("Displaced Threshold", threshold, "Threshold > 0");
 
 		this.disThreshold = threshold;
 	}
 
-	private void setStopway(double stopway) throws DistanceDeclarationException{
-		if( stopway < 0 ) throw new DistanceDeclarationException("Stopway", stopway, "Stopway >= 0");
+	private void setStopway(double stopway) throws VariableDeclarationException{
+		if( stopway < 0 ) throw new VariableDeclarationException("Stopway", stopway, "Stopway >= 0");
 	}
 
-	private void setClearway(double clearway) throws DistanceDeclarationException{
-		if( clearway < 0 ) throw new DistanceDeclarationException("Clearway", clearway, "Clearway > 0");
-		if( clearway < getStopway() ) throw new DistanceDeclarationException("Clearway", clearway, "Clearway >= Stopway");
+	private void setClearway(double clearway) throws VariableDeclarationException{
+		if( clearway < 0 ) throw new VariableDeclarationException("Clearway", clearway, "Clearway > 0");
+		if( clearway < getStopway() ) throw new VariableDeclarationException("Clearway", clearway, "Clearway >= Stopway");
 
 		this.clearway = clearway;
 	}
