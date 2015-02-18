d9374404acfa5990ad5cbc20afbd2bac20705a25
Stefan <Stefan@152.78.102.18>
2015-02-18 13:19:35 +0000

    Same as previous

--- Runway_Redeclaration/src/Exceptions/DistanceDeclarationException.java
+++ Runway_Redeclaration/src/Exceptions/DistanceDeclarationException.java
@@ -4,12 +4,22 @@ public class DistanceDeclarationException extends Exception{
 	/** @Version 18/02/2015 */
 	private static final long serialVersionUID = 179922121914261148L;
 
-	public DistanceDeclarationException(String distanceName, int value, String constraintInequality){
+	private String distanceName;
+	private double incorrectValue;
+	
+	public DistanceDeclarationException(String distanceName, double value, String constraintInequality){
 		super("DistanceDeclarationException:\n"
 				+"The variable "+distanceName+" has the value: "+value
 				+"Which does not follow: "+constraintInequality);
-		
+		this.distanceName = distanceName;
+		this.incorrectValue = value;
 	}
 	
-
+	public double getIncorrectvalue(){
+		return this.incorrectValue;
+	}
+	
+	public String getDistanceName(){
+		return this.distanceName;
+	}
 }
