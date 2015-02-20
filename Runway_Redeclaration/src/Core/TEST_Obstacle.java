8b4b3801ee06e9f5797c000afaebba6755a97ea7
Stefan <Stefan@Stefan-2-Laptop>
2015-02-20 18:07:45 +0000

    Fully tested Obstacle via JUnit

---
+++ Runway_Redeclaration/src/Core/TEST_Obstacle.java
@@ -0,0 +1,37 @@
+package Core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TEST_Obstacle {
+	
+	public static final String name = "TestName";
+	public static final double radius = 69;
+	public static final double height = 666.666;
+
+	public Obstacle obj;
+	
+	@Before
+	public void init(){
+		this.obj = new Obstacle(name,radius,height);
+	}
+	
+	@Test
+	public void testName() {
+		assertEquals(name, this.obj.getName());
+	}
+
+	@Test
+	public void testRadius(){
+		assertEquals(radius, this.obj.getRadius(),0);
+	}
+	
+	@Test
+	public void testHeight(){
+		assertEquals(height, this.obj.getHeight(),0);
+	}
+	
+	
+}
