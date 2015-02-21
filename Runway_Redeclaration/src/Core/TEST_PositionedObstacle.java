beb1c6cd4bd328475c7ea4e217b5c89145f2f4d3
Stefan <Stefan@Stefan-2-Laptop>
2015-02-21 14:24:13 +0000

    Test PositionedObstacle

---
+++ Runway_Redeclaration/src/Core/TEST_PositionedObstacle.java
@@ -0,0 +1,40 @@
+package Core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TEST_PositionedObstacle {
+	
+	public static final Obstacle obj = new Obstacle("Jim",54.02,67.99);
+	public static final double dFromSmallAnge = 1994;
+
+	public PositionedObstacle posObj;
+	
+	@Before
+	public void setUp() {
+		this.posObj = new PositionedObstacle(obj, dFromSmallAnge);
+	}
+
+	@Test
+	public void testDistance() {
+		assertEquals(dFromSmallAnge, this.posObj.distanceFromEnd(),0);
+	}
+	
+	@Test
+	public void testName(){
+		assertEquals(obj.getName(),this.posObj.getName());
+	}
+	
+	@Test
+	public void testRadius(){
+		assertEquals(obj.getRadius(), this.posObj.getRadius(),0);
+	}
+	
+	@Test
+	public void testHeight(){
+		assertEquals(obj.getHeight(), this.posObj.getHeight(),0);
+	}
+
+}
