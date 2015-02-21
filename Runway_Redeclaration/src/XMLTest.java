d13a79cd84c6cf783c7ee3c7d155e68bb75eacae
Jonathan Pepler <jp13g13@soton.ac.uk>
2015-02-21 23:03:01 +0000

    XML stuff.

--- Runway_Redeclaration/src/XMLTest.java
+++ Runway_Redeclaration/src/XMLTest.java
@@ -8,27 +8,18 @@ import java.util.ArrayList;
 import org.simpleframework.xml.Serializer;
 import org.simpleframework.xml.core.Persister;
 
+import Core.Airport;
 import Core.Obstacle;
+import Exceptions.CannotMakeRunwayException;
+import Exceptions.ParrallelRunwayException;
+import Exceptions.VariableDeclarationException;
 
 public class XMLTest {
 
 	public static void main(String[] args) {
-		Obstacle o = new Obstacle("Brian", 12, 13);
+		Obstacle o = new Obstacle("Bob", 0, 0);
 		FileSystem fs = new FileSystem();
 		System.out.println(fs.saveObs(o));
-		
-		ArrayList<File> files = fs.listObstacles();
-		System.out.println("Files:");
-		for(File file : files){
-			System.out.println(file.getName());
-		}
-		
-		String brian = files.get(0).getName();
-		System.out.println("\nAttempting to load " + brian + "...\n");
-		
-		Obstacle o2 = fs.loadObs(brian);
-		
-		System.out.println("Obstacle loaded. \nName: " + o2.getName() + "\nHeight: " + o2.getHeight() + "\nRadius: " + o2.getRadius());
 	}
 
 	static String readFile(String path) 
