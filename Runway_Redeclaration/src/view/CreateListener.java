2ed9c93beeda41feeb11778efdeb85c973c6080b
Vlad Catrici <vc4g13@soton.ac.uk>
2015-02-22 14:38:59 +0000

    Created Form class

--- Runway_Redeclaration/src/view/CreateListener.java
+++ Runway_Redeclaration/src/view/CreateListener.java
@@ -1,5 +1,11 @@
 package view;
 
 public class CreateListener {
+	TopFrame topFrame;
+	
+	public CreateListener(TopFrame topFrame){
+		this.topFrame = topFrame;
+	}
 
+	
 }
---
+++ Runway_Redeclaration/src/view/FormGeneral.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.swing.JDialog;
+
+
+public class FormGeneral extends JDialog {
+	
+	public static void main(String[] args) {
+		FormGeneral fg = new FormGeneral();
+		fg.
+	}
+
+}
