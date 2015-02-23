520385fe47f650f8025dc1cbd4246521b15efa52
Jonathan Pepler <jp13g13@soton.ac.uk>
2015-02-23 15:24:09 +0000

    Notification changes.

--- Runway_Redeclaration/src/io/XMLSaver.java
+++ Runway_Redeclaration/src/io/XMLSaver.java
@@ -5,7 +5,13 @@ import org.simpleframework.xml.Serializer;
 import org.simpleframework.xml.core.Persister;
 
 
+
+
+import Core.Airport;
+import Core.ParrallelRunwayException;
 import CoreInterfaces.Savable;
+import Exceptions.CannotMakeRunwayException;
+import Exceptions.VariableDeclarationException;
 /**
  * 
  * Serialises objects with XML
@@ -14,6 +20,21 @@ import CoreInterfaces.Savable;
  */
 
 public class XMLSaver {
+	
+	public static void main(String[] args) {
+		Airport a = new Airport("Gatwick");
+		double[] dimensions = {14,16,123,16,34,216,357,124};
+		try {
+			a.addNewAirfield(65, dimensions);
+		} catch (ParrallelRunwayException | CannotMakeRunwayException
+				| VariableDeclarationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		FileSystem fs = new FileSystem();
+		fs.saveAir(a);
+	}
 
 	public static boolean serialise(Savable object, String dir){
 		Serializer serializer = new Persister();
