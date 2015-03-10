9972bd64dad12b5274446826f4d2e7b2a173a6e6
Shakib-Bin Hamid <sh3g12@soton.ac.uk>
2015-03-10 17:05:08 +0000

    Changed Logs

--- Runway_Redeclaration/src/view/Notification.java
+++ Runway_Redeclaration/src/view/Notification.java
@@ -1,6 +1,5 @@
 package view;
 
-import java.awt.Color;
 
 public class Notification {
 	static TopFrame frame;
@@ -8,16 +7,9 @@ public class Notification {
 	public static void setFrame(TopFrame newFrame){
 		frame = newFrame;
 	}
-
-	public static void notify(String s){
-		frame.getLogPanel().notify(s);
-	}
 	
 	public static void notify(String s, String c){
 		frame.getLogPanel().notify(s, c);
 	}
-	
-	public static void notify(String s, Color c){
-		frame.getLogPanel().notify(s, c);
-	}
+
 }
