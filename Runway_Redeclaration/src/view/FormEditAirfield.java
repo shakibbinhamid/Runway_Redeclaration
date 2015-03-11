a01146303d2aeb631816b4281fe5276430d3297a
Vlad Catrici <vlad.catrici@gmail.com>
2015-03-11 12:49:20 +0000

    Overloaded setListener method in FormEditAirfield.

--- Runway_Redeclaration/src/view/FormEditAirfield.java
+++ Runway_Redeclaration/src/view/FormEditAirfield.java
@@ -1,9 +1,12 @@
 package view;
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 import Core.Airfield;
@@ -13,26 +16,26 @@ import Exceptions.VariableDeclarationException;
 public class FormEditAirfield extends FormAirfield{
 	Airfield currentAirfield;
 	
-//	// TESTING 
-//	public static void main(String[] args) {
-//		test();
-//	}
-//	
-//	public static void test(){
-//		double[] testSmallValues = {3500,4000,4500,2500};
-//		double[] testBigValues = {4000,4500,5000,3000};
-//		TopFrame tf = new TopFrame();
-//		Airport airp = new Airport("Gatwicked");
-//		tf.loadOrCreateAirport(airp);
-//		try {
-//			tf.loadOrCreateField(90, ' ', testSmallValues, testBigValues);
-//			tf.loadOrCreateAirport(airp);
-//		} catch (VariableDeclarationException e) {
-//			e.printStackTrace();
-//		}
-//		
-//		FormEditAirfield fa = new FormEditAirfield(tf);
-//	}
+	// TESTING 
+	public static void main(String[] args) {
+		test();
+	}
+	
+	public static void test(){
+		double[] testSmallValues = {3500,4000,4500,2500};
+		double[] testBigValues = {4000,4500,5000,3000};
+		TopFrame tf = new TopFrame();
+		Airport airp = new Airport("Gatwicked");
+		tf.loadOrCreateAirport(airp);
+		try {
+			tf.loadOrCreateField(90, ' ', testSmallValues, testBigValues);
+			tf.loadOrCreateAirport(airp);
+		} catch (VariableDeclarationException e) {
+			e.printStackTrace();
+		}
+		
+		FormEditAirfield fa = new FormEditAirfield(tf);
+	}
 	
 	public FormEditAirfield(TopFrame topFrame){
 		super(topFrame, "Edit airfield");
@@ -95,5 +98,74 @@ public class FormEditAirfield extends FormAirfield{
 				}
 			});
 		}
+	}
+	
+
+	public void setListener(){
+		
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				boolean okToAdd = false;
+				try{
+					if(angleTextBox.getText().equals("") || Integer.parseInt(angleTextBox.getText()) < 0){
+						JOptionPane.showMessageDialog(null, "Insert a valid angle value!", "Error!", JOptionPane.ERROR_MESSAGE);
+					}
+				} catch (NumberFormatException n) {
+					JOptionPane.showMessageDialog(null, "Insert a valid angle value!", "Error!", JOptionPane.ERROR_MESSAGE);
+				}
+				
+				double[] smallInputs = new double[4];
+				double[] bigInputs = new double[4];
+				int angle = Integer.parseInt(angleTextBox.getText());
+				String sideStrig= (String) sideComboBox.getSelectedItem();
+				char sideChar = sideStrig.charAt(0);
+				
+				if(Integer.parseInt(angleTextBox.getText()) > 0){
+					for (int i = 0; i < 4; i++) {
+						try {
+							if (Integer.parseInt(smallValueTextFields.get(i).getText()) < 0
+									|| Integer.parseInt(bigValueTextFields.get(i).getText()) < 0
+									|| smallValueTextFields.get(i).getText().equals("")
+									|| bigValueTextFields.get(i).getText().equals("")) {
+								System.err.println("Invalid inputs!");
+								JOptionPane.showMessageDialog(null,
+										"Please input only integers!",
+										"Invalid Input!",
+										JOptionPane.ERROR_MESSAGE);
+								break;
+							} else {
+								okToAdd = true;
+								smallInputs[i] = Integer
+										.parseInt(smallValueTextFields.get(i)
+												.getText());
+								bigInputs[i] = Integer.parseInt(bigValueTextFields
+										.get(i).getText());
+							}
+						} catch (NumberFormatException e) {
+							JOptionPane.showMessageDialog(null,
+									"Please input only integers!",
+									"Invalid Input!", JOptionPane.ERROR_MESSAGE);
+							break;
+						}
+					}
+				
+					if (okToAdd)
+						try {
+							angle = Integer.parseInt(angleTextBox.getText());
+							sideStrig = (String) sideComboBox.getSelectedItem();
+							sideChar = sideStrig.charAt(0);
+							topFrame.getTabbePanel().updateTab(new Airfield(angle, sideChar,
+									smallInputs, bigInputs));
+							dispose();
+						} catch (NumberFormatException e) {
+							JOptionPane.showMessageDialog(null, e.getMessage(),
+									"Invalid Input!", JOptionPane.ERROR_MESSAGE);
+						} catch (VariableDeclarationException e) {
+							JOptionPane.showMessageDialog(null, e.getMessage(),
+									"Invalid Input!", JOptionPane.ERROR_MESSAGE);
+						}
+				}
+			}
+		});
 	}
 }
