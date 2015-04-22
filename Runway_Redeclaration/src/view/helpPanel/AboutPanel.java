03c0d769dbe76d71f59dec8c59e6cf94b790b350
Jonathan Pepler <jp13g13@soton.ac.uk>
2015-04-22 12:48:50 +0100

    Adding image to the About page.

--- Runway_Redeclaration/src/view/helpPanel/AboutPanel.java
+++ Runway_Redeclaration/src/view/helpPanel/AboutPanel.java
@@ -3,6 +3,7 @@ package view.helpPanel;
 import java.awt.GridLayout;
 import java.io.File;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -14,9 +15,11 @@ public class AboutPanel extends JFrame{
 
 	private static final String aboutDir = "./about.txt";
 	private static final String licenseDir = "./ccl.txt";
+	private static final String photoDir = "./groupphoto.jpg";
+	private static final int scaler = 250;
 	
 	private String aboutText, licenseText;
-	private JLabel html, about;
+	private JLabel html, about, photo;
 	
 	public static void main(String[] args) {
 		new AboutPanel();
@@ -27,6 +30,9 @@ public class AboutPanel extends JFrame{
 		FileSystem fs = new FileSystem();
 		aboutText = fs.getTextFromFile(new File(aboutDir));
 		licenseText = fs.getTextFromFile(new File(licenseDir));
+		photo = new JLabel();
+		photo.setIcon(new ImageIcon(ImageTools.getScaledImage((new ImageIcon(photoDir).getImage()), scaler)));
+		
 		
 		html = new JLabel();
 		html.setText(licenseText);
@@ -36,13 +42,14 @@ public class AboutPanel extends JFrame{
 		
 		JPanel panel = new JPanel();
 		
-		panel.setLayout(new GridLayout(1,1));
+		panel.setLayout(new GridLayout(1,2));
 		panel.setBorder(new EmptyBorder(10,10,10,10));
 		panel.add(html);
+		panel.add(photo);
 		this.add(panel);
 		
 		this.setTitle("About - " + aboutText);
-		this.setSize(500, 200);
+		this.setSize(550, 250);
 		this.setVisible(true);
 		
 	}
